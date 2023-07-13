5d9cfcd4f9f9b82e075d8e43a60b142e6b2c7a09
Alex Smith <alexmcraysmith@gmail.com>
2023-07-12 18:14:29 -0600

    Add Invoice Update Endpoint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-from flask import Flask, jsonify
+from flask import Flask, abort, jsonify, make_response, request
 from flaskr.db import get_db
 
 
@@ -45,9 +45,34 @@ def create_app(test_config=None):
         ).fetchall()
         return query_to_json_response(invoices)
 
-    @app.put('/api/v1/invoice/<int:invoice_id>')
-    def update_invoice(borrower_id):
-        return 'Put invoice'
+    @app.put('/api/v1/invoices/<int:invoice_id>')
+    def update_invoice(invoice_id):
+        db = get_db()
+        error = None
+
+        invoice = db.execute(
+            'SELECT * FROM invoice WHERE id = ?', (invoice_id,)
+        ).fetchone()
+
+        if invoice is None:
+            abort(make_response(jsonify(message="Invoice id {invoice_id} doesn't exist."), 404))
+
+        data = request.get_json()
+        state = data.get('invoice').get('state')
+
+        if not state:
+            abort(make_response(jsonify(message="Invoice state is required."), 400))
+
+        db.execute(
+            'UPDATE invoice SET state = ?'
+            ' WHERE id = ?',
+            (state, invoice_id)
+        )
+        db.commit()
+        updated_row = dict(invoice)
+        updated_row['state'] = state
+
+        return jsonify(updated_row)
 
     from . import db
     db.init_app(app)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,17 +1,40 @@
+import json
 import pytest
+
+from flask import url_for
 from flaskr.db import get_db
+from flaskr import create_app
 
 
-def test_borrowers_index(client):
-    response = client.get('/borrowers')
-    assert b"John Smith" in response.data
-    assert b"Mart Mart LLC" in response.data
+def test_borrowers_index(app, client):
+    with app.app_context(), app.test_request_context():
+        response = client.get(url_for('borrowers'))
+        assert b"John Smith" in response.data
+        assert b"Mart Mart LLC" in response.data
 
-def test_invoices_index(client):
-    response = client.get('/borrowers/1/invoices')
-    assert b"JS-1000" in response.data
-    assert b"JS-1001" in response.data
+def test_invoices_index(app, client):
+    with app.app_context(), app.test_request_context():
+        response = client.get(url_for('borrower_invoices', borrower_id=1))
+        assert b"JS-1000" in response.data
+        assert b"JS-1001" in response.data
 
-def test_blank_invoices_index(client):
-    response = client.get('/borrowers/4/invoices')
-    assert b"[]" in response.data
+def test_blank_invoices_index(app, client):
+    with app.app_context(), app.test_request_context():
+        response = client.get(url_for('borrower_invoices', borrower_id=4))
+        assert b"[]" in response.data
+
+def test_put_invoice(app, client):
+    with app.app_context(), app.test_request_context():
+        tested_id = 1
+        new_status = 'approved'
+        data = { 'status': new_status }
+        headers = {'Content-Type': 'application/json'}
+
+        response = client.put(url_for('update_invoice', invoice_id=tested_id), data=json.dumps(data), headers=headers)
+
+        updated = get_db().execute(
+            'SELECT * FROM invoice WHERE id = ?', (tested_id,)
+        ).fetchone()
+
+        assert response.status_code == 200
+        assert updated['state'] == new_status
